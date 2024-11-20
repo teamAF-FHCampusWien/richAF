741ec9bf737a514327124a3668c4c5aab37204f6
TheBloodyAmateur <botan_kadir@outlook.com>
2024-11-20 21:36:21 +0100

    See if the connectivity with GitHub works!

--- src/main/java/at/ac/fhcampuswien/richAF/Main.java
+++ src/main/java/at/ac/fhcampuswien/richAF/Main.java
@@ -2,7 +2,10 @@ package at.ac.fhcampuswien.richAF;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("pushing to new repo!");
+        testMethod();
+    }
+
+    public static void testMethod(){
+        System.out.print("Hello world");
     }
 }
