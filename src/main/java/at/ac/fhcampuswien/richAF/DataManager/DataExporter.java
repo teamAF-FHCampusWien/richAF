a0d50dc8a1142807dbf523115ccf46833500bc77
TheBloodyAmateur <botan_kadir@outlook.com>
2024-11-21 12:26:02 +0100

    Expanding the DataExporter class, no actual implementation, just brainstorming

--- src/main/java/at/ac/fhcampuswien/richAF/DataManager/DataExporter.java
+++ src/main/java/at/ac/fhcampuswien/richAF/DataManager/DataExporter.java
@@ -2,4 +2,12 @@ package at.ac.fhcampuswien.richAF.DataManager;
 
 public class DataExporter {
 
+    public static String exportCSV(){
+        return "Placeholder";
+    }
+
+    public static String exportJSON(){
+        return "Placeholder";
+    }
+
 }
