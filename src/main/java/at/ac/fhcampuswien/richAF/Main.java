0c2a91cd0f85bc5a3a30e5b9906e7b0c4baefc4a
TheBloodyAmateur <botan_kadir@outlook.com>
2024-11-23 13:11:01 +0100

    Working on the logging mechanism

--- src/main/java/at/ac/fhcampuswien/richAF/Main.java
+++ src/main/java/at/ac/fhcampuswien/richAF/Main.java
@@ -1,7 +1,10 @@
 package at.ac.fhcampuswien.richAF;
 
+import at.ac.fhcampuswien.richAF.data.EventManager;
+
 public class Main {
     public static void main(String[] args) {
-
+        EventManager eventManager = new EventManager("log.txt");
+        eventManager.logMessage("Hello World!",Main.class.getName());
     }
 }
