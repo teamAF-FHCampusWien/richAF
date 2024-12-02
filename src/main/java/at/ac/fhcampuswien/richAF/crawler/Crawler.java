0c18e35d0898640f056d5f075542c4932e21fdac
Clemens <>
2024-12-02 19:33:22 +0100

    [FEAT] Basic webpage crawling and mesh generation done

--- src/main/java/at/ac/fhcampuswien/richAF/crawler/Crawler.java
+++ src/main/java/at/ac/fhcampuswien/richAF/crawler/Crawler.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @Log4j2
 @Getter
@@ -21,11 +19,11 @@ public class Crawler {
     private int maxDepth = 2;
     private int currentDepth = 0;
 
-    public Crawler(String uri) throws IOException, URISyntaxException {
+    public Crawler(String uri) {
         this.currentNode = this.mesh.createNode(new Page(URI.create(uri)), this.currentNode);
     }
 
-    public Crawler(String uri, int maxDepth) throws IOException, URISyntaxException {
+    public Crawler(String uri, int maxDepth) {
         this.currentNode = this.mesh.createNode(new Page(URI.create(uri)), this.currentNode);
         this.maxDepth = maxDepth;
     }
@@ -35,7 +33,7 @@ public class Crawler {
     // 2: Check if mesh already contains some of the page links (those can be omitted)
     // 3: Add node of generated page to mesh
     // TODO:    4: Crawl all remaining page links and start from the top
-    public void crawl() throws IOException, URISyntaxException {
+    public void crawl() {
         currentDepth++;
         if (this.currentDepth == this.maxDepth) return;
         for (URI link : this.currentNode.getPage().getLinks()) {
