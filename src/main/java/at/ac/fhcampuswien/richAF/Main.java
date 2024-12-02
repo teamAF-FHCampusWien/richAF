bdc6f207ab254be3ada521de3bfeb098a1fc744c
Clemens <>
2024-12-02 19:33:22 +0100

    [FEAT] Basic webpage crawling and mesh generation done

--- src/main/java/at/ac/fhcampuswien/richAF/Main.java
+++ src/main/java/at/ac/fhcampuswien/richAF/Main.java
@@ -6,20 +6,13 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 @Log4j2
 public class Main {
     public static void main(String[] args) {
         Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.DEBUG);
-        try {
-            Crawler crawler = new Crawler("https://fh-campuswien.ac.at");
-            crawler.crawl();
-            log.info("Crawl complete.");
-            log.info("Created mesh with {} nodes", crawler.getMesh().getNodes().size());
-        } catch (IOException | URISyntaxException e) {
-           log.error(e);
-        }
+        Crawler crawler = new Crawler("https://fh-campuswien.ac.at");
+        crawler.crawl();
+        log.info("Crawl complete.");
+        log.info("Created mesh with {} nodes", crawler.getMesh().getNodes().size());
     }
 }
