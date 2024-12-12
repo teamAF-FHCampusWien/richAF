dbb11f9162340a1dec4481396a613335e0aa1230
TheBloodyAmateur <botan_kadir@outlook.com>
2024-12-12 18:48:44 +0100

    Add the json module

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.json:json:20171018")
+    implementation("org.json:json:20231013")
 }
 
 tasks.test {
