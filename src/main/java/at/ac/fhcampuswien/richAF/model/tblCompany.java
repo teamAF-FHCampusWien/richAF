820117aeacc7ff4aac29abc865aafb5191a42fad
stefanhFH <stefan.herzig@stud.fh-campuswien.ac.at>
2024-12-26 18:02:59 +0100

    adding Documentation

--- src/main/java/at/ac/fhcampuswien/richAF/model/tblCompany.java
+++ src/main/java/at/ac/fhcampuswien/richAF/model/tblCompany.java
@@ -2,16 +2,30 @@ package at.ac.fhcampuswien.richAF.model;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-
+/**
+ * Model Class for the DAO Table Models
+ * corresponding Table tblCompany
+ * containing properties with their Getters and Setters
+ * properties/tablecolumns:
+ *      id
+ *      strName
+ * @author Stefan
+ */
 @DatabaseTable(tableName = "tblCompany")
 public class tblCompany {
     public tblCompany() {
 
     }
 
+    /**
+     * id ...  autoincrement integer value
+     */
     @DatabaseField(generatedId = true)
     private int id;
 
+    /**
+     * strName ... Name of the Company
+     */
     @DatabaseField
     private String strName;
     public int getId() {
--- src/main/java/at/ac/fhcampuswien/richAF/model/tblPage.java
+++ src/main/java/at/ac/fhcampuswien/richAF/model/tblPage.java
@@ -3,16 +3,36 @@ package at.ac.fhcampuswien.richAF.model;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-import java.sql.Timestamp;
-
+/**
+ * Model Class for the DAO Table Models
+ * corresponding Table tblPages
+ * containing properties with their Getters and Setters
+ * properties/tablecolumns:
+ *      id
+ *      strPage
+ *      strKeyword
+ *      intStatus from baseTbl
+ *      tsCreated_on from baseTbl
+ * @author Stefan
+ */
 @DatabaseTable(tableName = "tblPages")
 public class tblPage extends baseTbl {
+    /**
+     * id ...  autoincrement integer value
+     */
     @DatabaseField(generatedId = true)
     private int id;
 
+    /**
+     * strPage ... the webpage html text from the webcrawler
+     */
     @DatabaseField
     private String strPage;
 
+    /**
+     * strKeyword ... a keyword / company name which this page is explicit referred to
+     * value "" means that this webpage is for all companies relevant
+     */
     @DatabaseField
     private String strKeyword;
 
