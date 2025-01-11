73e4ba4200e51a3bcba4151742858dfc0deee8c5
Timmo Achsel <achseltimmo@gmail.com>
2025-01-11 18:06:01 +0100

    change resolution to 1920x1080p for scenebuilder scene

--- src/main/java/at/ac/fhcampuswien/richAF/App.java
+++ src/main/java/at/ac/fhcampuswien/richAF/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/app.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setScene(scene);
         stage.show();
     }
