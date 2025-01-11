3addf2baf7872630fed48bde87d82bd5ce276b1e
Timmo Achsel <achseltimmo@gmail.com>
2025-01-11 16:48:52 +0100

    fix minor errors, remove unused code

--- src/main/java/at/ac/fhcampuswien/richAF/App.java
+++ src/main/java/at/ac/fhcampuswien/richAF/App.java
@@ -1,7 +1,5 @@
 package at.ac.fhcampuswien.richAF;
 
-import at.ac.fhcampuswien.richAF.controller.Controller;
-import at.ac.fhcampuswien.richAF.data.EventManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,7 +13,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/test.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/app.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 400);
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/at/ac/fhcampuswien/richAF/view/Dashboard.java
@@ -0,0 +1,21 @@
+package at.ac.fhcampuswien.richAF.view;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Dashboard extends Application {
+
+    @Override
+    public void start(Stage stage) {
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
